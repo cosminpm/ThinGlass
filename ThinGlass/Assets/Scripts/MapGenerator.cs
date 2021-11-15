2e4b2ddf125463bd933fea695c55068d890d539c
Cosmin <cosminmarian.paduraru@gmail.com>
2021-11-15 21:07:04 +0100

    Generation of island
    Done the initial map of the island

--- ThinGlass/Assets/Scripts/MapGenerator.cs
+++ ThinGlass/Assets/Scripts/MapGenerator.cs
@@ -5,36 +5,37 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    
-   
-    private int _width = 5;
-    private int _height = 3;
+    private int _width = 10;
+    private int _height = 10;
     private GameObject[,] _arrOfPlanes;
+    private float _scaler = 0.15f;
+    private float _widthPlane;
+    private float _heightPlane;
+    
     void Start()
     {
         _arrOfPlanes = new GameObject[_width, _height];
-            
-        for (int i = 0; i < _width; i++)
-            {
-                for (int j = 0; j < _height; j++)
-                {
-                    _arrOfPlanes[i, j] = GameObject.CreatePrimitive(PrimitiveType.Plane);
-                    _arrOfPlanes[i, j].gameObject.transform.localScale = new Vector3(1, 1, 1);
-                    
-                    float heightPlane = _arrOfPlanes[i, j].gameObject.GetComponent<MeshFilter>().mesh.bounds.extents.z * 2;
-                    float widthPlane = _arrOfPlanes[i, j].gameObject.GetComponent<MeshFilter>().mesh.bounds.extents.x * 2;
-                    float positionX = i * widthPlane;
-                    float positionZ = j * heightPlane;
-                    
-                    //_arrOfPlanes[i, j].gameObject.transform.position = new Vector3(positionX, 0, positionZ);
-                    _arrOfPlanes[i ,j].gameObject.transform.SetParent(gameObject.transform);
 
-                    if (Mathf.PerlinNoise(positionX, positionZ) > 0.3 + 0.4 * distance_squared(positionX, positionZ))
-                    {
-                        _arrOfPlanes[i, j].gameObject.transform.position = new Vector3(positionX, 0, positionZ);
-                    }
-                }
+        _widthPlane = _getSizeOfPlane(1f, 1f)[0];
+        _heightPlane = _getSizeOfPlane(1f, 1f)[1];
+
+        for (int i = 0; i < _width; i++)
+        for (int j = 0; j < _height; j++)
+        {
+            // Get the position of where should be the object
+            float positionX = i * _widthPlane;
+            float positionZ = j * _heightPlane;
+
+            float perlinNoiseValue = Mathf.PerlinNoise(positionX * _scaler, positionZ * _scaler);
+
+            if (perlinNoiseValue > 0.3 + 0.1 * distance_squared(positionX, positionZ))
+            {
+                _arrOfPlanes[i, j] = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                _arrOfPlanes[i, j].gameObject.transform.localScale = new Vector3(1, 1, 1);
+                _arrOfPlanes[i, j].gameObject.transform.SetParent(gameObject.transform);
+                _arrOfPlanes[i, j].gameObject.transform.position = new Vector3(positionX, 0, positionZ);
             }
+        }
     }
 
     // Update is called once per frame
@@ -42,11 +43,23 @@ public class MapGenerator : MonoBehaviour
     {
         
     }
-
+    
+    // Return the size of a plane given the scale of x and z
+    private float[] _getSizeOfPlane(float x, float z)
+    {
+        var aux = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        aux.transform.localScale = new Vector3(1, 1, 1);
+        float heightPlane = aux.GetComponent<MeshFilter>().mesh.bounds.extents.z * 2;
+        float widthPlane = aux.GetComponent<MeshFilter>().mesh.bounds.extents.x * 2;
+        Destroy(aux);
+        return new[] {widthPlane, heightPlane};
+    }
+    
+    // Get the distanced squared from the center, this function is used to make a map
     private float  distance_squared(float x, float y)
     {
-        float dx = 2 * x / _width - 1;
-        float dy = 2 * y / _height - 1;
+        float dx = 2 * x / (_width*_getSizeOfPlane(1f, 1f)[0]) - 1;
+        float dy = 2 * y / (_height*_getSizeOfPlane(1f, 1f)[1]) - 1;
         //at this point 0 <= dx <= 1 and 0 <= dy <= 1
         return dx * dx + dy * dy;
     }
