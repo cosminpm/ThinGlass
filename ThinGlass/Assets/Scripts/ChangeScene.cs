6e70ca105c00cdc0a8f99cbaa27c9dbae1d3a6aa
Cosmin <cosminmarian.paduraru@gmail.com>
2021-11-18 18:11:41 +0100

    Only Help needed scene

---
+++ ThinGlass/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ButtonChangeScene(string scene)
+    {
+        StartCoroutine(DelaySceneLoad(scene));
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    IEnumerator DelaySceneLoad(string scene)
+    {
+        
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(scene);
+    }
+}
